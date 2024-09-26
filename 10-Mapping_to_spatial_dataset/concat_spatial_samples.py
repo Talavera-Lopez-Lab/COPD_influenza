36131587abc919230cbbf4a3fe48147440a69d2d
egerc <eger.christian@gmx.de>
2024-09-26 19:16:11 +0200

    added filtering of cells and genes

--- 10-Mapping_to_spatial_dataset/concat_spatial_samples.py
+++ 10-Mapping_to_spatial_dataset/concat_spatial_samples.py
@@ -17,6 +17,8 @@ for adata in adatas:
 adata = sc.concat(adatas)
 adata.obs = adata.obs.reset_index().merge(metadata, on='sample_id', how='left').set_index('index')
 adata.obs_names_make_unique()
+sc.pp.filter_cells(adata, min_counts=200)
+sc.pp.filter_genes(adata, min_cells=3)
 
 filename = 'Franzen_L_2024_human.h5ad'
 adata.write_h5ad(local_folder_data / filename)
