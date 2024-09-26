243929604c1a516d21fe6864a1ef6d4d57890d09
egerc <eger.christian@gmx.de>
2024-09-26 15:24:01 +0200

    metadata annotation works now

--- 10-Mapping_to_spatial_dataset/concat_spatial_samples.py
+++ 10-Mapping_to_spatial_dataset/concat_spatial_samples.py
@@ -1,17 +1,23 @@
 from pathlib import Path
 import scanpy as sc
 import squidpy as sq
+import pandas as pd
 
 local_folder_data = Path.cwd() / '.data'
+metadata = pd.read_csv(local_folder_data / 'hs_visium_metadata.tsv', sep='\t', index_col='sample_id')
 
-adatas = [sq.read.visium(folder) for folder in (local_folder_data / 'raw').iterdir()]
+adatas = []
+for folder in (local_folder_data / 'raw').iterdir():
+    adata = sq.read.visium(folder)
+    adata.obs['sample_id'] = folder.name
+    adatas.append(adata)
 print('samples Loaded')
 for adata in adatas:
     adata.var_names_make_unique()
 adata = sc.concat(adatas)
+adata.obs = adata.obs.reset_index().merge(metadata, on='sample_id', how='left').set_index('index')
 adata.obs_names_make_unique()
-del adatas
 
-filename = 'Franzen_L_2024_humand.h5ad'
+filename = 'Franzen_L_2024_human.h5ad'
 adata.write_h5ad(local_folder_data / filename)
 print(f'{filename} written')
