e5eb76ed6f606e28031456c19e33f5b5a7509a93
egerc <eger.christian@gmx.de>
2024-10-02 08:28:52 +0200

    .

--- 10-Mapping_to_spatial_dataset/0-seurat_to_h5ad.R
+++ 10-Mapping_to_spatial_dataset/0-seurat_to_h5ad.R
@@ -1,5 +1,7 @@
+# Using seruat_to_h5ad-env environment
 library(sceasy)
 library(reticulate)
 seurat_object <- readRDS(".data/GSE135893_ILD_annotated_fullsize.rds")
+seurat_object@images <- list()
 sceasy::convertFormat(seurat_object, from="seurat", to="anndata",
-                       outFile='GSE135893_ILD_annotated_fullsize')
+                       outFile='.data/GSE135893_ILD_annotated_fullsize.h5ad')
